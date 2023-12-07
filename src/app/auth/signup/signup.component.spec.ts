c9013cf80e99121c3950766f7099166e052604c4
CutieLich <112940272+qtlich@users.noreply.github.com>
2023-12-07 23:54:46 +0200

    Update signup.component.spec.ts

--- src/app/auth/signup/signup.component.spec.ts
+++ src/app/auth/signup/signup.component.spec.ts
@@ -1,7 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SignupComponent } from './signup.component';
-
 describe('SignupComponent', () => {
   let component: SignupComponent;
   let fixture: ComponentFixture<SignupComponent>;
