ab8ba6aba4be449b9099d85f2efa2483b2a548e2
CutieLich <112940272+qtlich@users.noreply.github.com>
2023-12-07 23:58:35 +0200

    Update side-bar.component.spec.ts

--- src/app/shared/side-bar/side-bar.component.spec.ts
+++ src/app/shared/side-bar/side-bar.component.spec.ts
@@ -1,7 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SideBarComponent } from './side-bar.component';
-
 describe('SideBarComponent', () => {
   let component: SideBarComponent;
   let fixture: ComponentFixture<SideBarComponent>;
