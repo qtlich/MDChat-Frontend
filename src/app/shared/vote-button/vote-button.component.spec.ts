53286298f455a95166b56e8245fcd871ac3c439b
CutieLich <112940272+qtlich@users.noreply.github.com>
2023-12-07 23:58:59 +0200

    Update vote-button.component.spec.ts

--- src/app/shared/vote-button/vote-button.component.spec.ts
+++ src/app/shared/vote-button/vote-button.component.spec.ts
@@ -1,7 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { VoteButtonComponent } from './vote-button.component';
-
 describe('VoteButtonComponent', () => {
   let component: VoteButtonComponent;
   let fixture: ComponentFixture<VoteButtonComponent>;
